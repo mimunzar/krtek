57bb76a4aae009414393eff0ddc5e0104156f312
Milan Munzar <mimunzar@gmail.com>
2021-05-16 16:19:34 +0200

    Adds Unit Test for Screen and Tmux Commands

--- tslime-test.el
+++ tslime-test.el
@@ -1,9 +1,36 @@
-(ert-deftest krtek-str-join-test ()
-  (should (equal "" (krtek-str-join "" '())))
-  (should (equal "foo" (krtek-str-join "x" '("foo"))))
-  (should (equal "fooxbar" (krtek-str-join "x" '("foo" "bar")))))
+(require 'tslime)
 
-(ert-deftest krtek-append-newline-if-missing-test ()
-  (should (equal "\n" (krtek-append-newline-if-missing "")))
-  (should (equal "\n\n" (krtek-append-newline-if-missing "\n\n")))
-  (should (equal "\n\nfoo\n" (krtek-append-newline-if-missing "\n\nfoo"))))
+
+(ert-deftest tslime-str-join-test ()
+  (should (equal "" (tslime-str-join "" '())))
+  (should (equal "foo" (tslime-str-join "x" '("foo"))))
+  (should (equal "fooxbar" (tslime-str-join "x" '("foo" "bar")))))
+
+(ert-deftest tslime-append-newline-if-missing-test ()
+  (should (equal "\n" (tslime-append-newline-if-missing "")))
+  (should (equal "\n\n" (tslime-append-newline-if-missing "\n\n")))
+  (should (equal "\n\nfoo\n" (tslime-append-newline-if-missing "\n\nfoo"))))
+
+(ert-deftest tslime-paste-buffer-to-tmux-panel-test ()
+  (let ((result '())
+        (fn-shell-command (lambda (x) (push x result))))
+    (tslime-paste-buffer-to-tmux-panel "buffer" "panel" fn-shell-command)
+    (should (equal "tmux -L default paste-buffer -d -t panel" (car result)))
+    (should (equal "tmux -L default load-buffer buffer" (cadr result)))))
+
+(ert-deftest tslime-paste-buffer-to-screen-panel-test ()
+  (let ((result '())
+        (fn-shell-command (lambda (x) (push x result))))
+    (tslime-paste-buffer-to-screen-panel "buffer" "panel" fn-shell-command)
+    (should (equal "screen -p panel -X paste p" (car result)))
+    (should (equal "screen -X eval 'readreg p buffer'" (cadr result)))))
+
+;; (ert-deftest tslime-parse-screen-session-list-test ()
+;;   (let ((output "No Sockets found in /run/screen/S-mmunzar."))
+;;     (should (equal '() (tslime-parse-screen-session-list output))))
+;;   (let ((output (tslime-str-join "\n" '("There are screens on:"
+;;                                         "      27900.pts-20.stolova-hora       (Attached)"
+;;                                         "      17403.pts-13.stolova-hora       (Attached)"
+;;                                         "2 Sockets in /run/screen/S-mmunzar.")))
+;;         (result '("27900.pts-20.stolova-hora" "17403.pts-13.stolova-hora")))
+;;     (should (equal result (tslime-parse-screen-session-list output)))))
--- tslime.el
+++ tslime.el
@@ -1,6 +1,7 @@
 (defvar tslime-multiplexer 'tmux)
 (defvar tslime-panel nil)
-(defvar tslime-buffer-file (make-temp-file "krtek"))
+(defvar tslime-buffer (make-temp-file "tslime"))
+(defvar tslime-screen-session nil)
 
 (defun tslime-str-join (separator seq)
   (mapconcat 'identity seq separator))
@@ -8,27 +9,33 @@
 (defun tslime-append-newline-if-missing (s)
   (replace-regexp-in-string "\n\n$" "\n" (concat s "\n")))
 
-(defun tslime-send-string-to-tmux-panel (s tslime-panel)
-  (with-temp-file tslime-buffer-file
-    (insert s))  
-  (shell-command (tslime-str-join " " (list "tmux -L default load-buffer" tslime-buffer-file)))
-  (shell-command (tslime-str-join " " (list "tmux -L default paste-buffer -d -t" tslime-panel))))
+(defun tslime-paste-buffer-to-tmux-panel (buffer panel fn-shell)
+  (funcall fn-shell (tslime-str-join " " (list "tmux -L default load-buffer" buffer)))
+  (funcall fn-shell (tslime-str-join " " (list "tmux -L default paste-buffer -d -t" panel))))
+
+(defun tslime-send-string-to-tmux-panel (s panel)
+  (let ((s (tslime-append-newline-if-missing s)))
+    (with-temp-file tslime-buffer (insert s))
+    (tslime-paste-buffer-to-tmux-panel tslime-buffer panel 'shell-command)))
 
 (defun tslime-send-string-tmux (s)
   (unless tslime-panel
     (setq tslime-panel (read-string "tmux target panel: " "{last}")))
-  (tslime-send-string-to-tmux-panel (tslime-append-newline-if-missing s) tslime-panel))
+  (tslime-send-string-to-tmux-panel s tslime-panel))
 
-(defun tslime-send-string-to-screen-panel (s tslime-panel)
-  (with-temp-file tslime-buffer-file
-    (insert s))
-  (shell-command (tslime-str-join " " (list "screen -X eval 'readreg p" (concat tslime-buffer-file "'"))))
-  (shell-command (tslime-str-join " " (list "screen -p" tslime-panel "-X paste p"))))
+(defun tslime-paste-buffer-to-screen-panel (file panel fn-shell)
+  (funcall fn-shell (tslime-str-join " " (list "screen -X eval 'readreg p" (concat file "'"))))
+  (funcall fn-shell (tslime-str-join " " (list "screen -p" panel "-X paste p"))))
+
+(defun tslime-send-string-to-screen-panel (s panel)
+  (let ((s (tslime-append-newline-if-missing s)))
+    (with-temp-file tslime-buffer (insert s))
+    (tslime-paste-buffer-to-tmux-panel tslime-buffer panel 'shell-command)))
 
 (defun tslime-send-string-screen (s)
   (unless tslime-panel
-    (setq tslime-panel (read-string "screen target panel: " "prev")))
-  (tslime-send-string-to-screen-panel (tslime-append-newline-if-missing s) tslime-panel))
+    (setq tslime-panel (read-string "screen target panel: " "1")))
+  (tslime-send-string-to-screen-panel s tslime-panel))
 
 (defun tslime-send-string-dispatch (s)
   (pcase tslime-multiplexer
@@ -54,3 +61,5 @@
   (setq tslime-panel nil))
 
 (global-set-key (kbd "C-c C-c") 'tslime-send)
+
+(provide 'tslime)

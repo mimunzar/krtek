aabeccaecde9b8afbe0101a87216588f6f4732a6
Milan Munzar <mimunzar@gmail.com>
2021-05-19 09:29:33 +0200

    Encloses Unnecessary Exposed Variables

--- tslime-test.el
+++ tslime-test.el
@@ -1,9 +1,9 @@
 (require 'tslime)
 
-(ert-deftest tslime-str-join-test ()
-  (should (equal "" (tslime-str-join "" '())))
-  (should (equal "foo" (tslime-str-join "x" '("foo"))))
-  (should (equal "fooxbar" (tslime-str-join "x" '("foo" "bar")))))
+(ert-deftest tslime-s-join-test ()
+  (should (equal "" (tslime-s-join "" '())))
+  (should (equal "foo" (tslime-s-join "x" '("foo"))))
+  (should (equal "fooxbar" (tslime-s-join "x" '("foo" "bar")))))
 
 (ert-deftest tslime-re-matches-test ()
   (should (equal nil (tslime-re-matches "turtle" "The quick brown fox")))
@@ -37,6 +37,23 @@
                   "      17403.pts-13.stolova-hora       (Attached)"
                   "2 Sockets in /run/screen/S-mmunzar.")))
     (should (equal '("27900.pts-20.stolova-hora" "17403.pts-13.stolova-hora")
-                   (tslime-parse-screen-session-list (tslime-str-join "\n" output))))))
+                   (tslime-parse-screen-session-list (tslime-s-join "\n" output))))))
 
+(ert-deftest tslime-make-send-multiplexer-test ()
+  (let* ((prompted-times 0)
+         (f-prompt-conf (lambda () (setq prompted-times (+ prompted-times 1)) "conf"))
+         (f-multiplexer-send (lambda (s conf) (list s conf)))
+         (f-send-multiplexer (tslime-make-send-multiplexer f-prompt-conf f-multiplexer-send)))
+    (should (equal '("foo" "conf") (funcall f-send-multiplexer "foo")))
+    (should (equal '("bar" "conf") (funcall f-send-multiplexer "bar")))
+    (should (equal 1 prompted-times))
+    (should (equal '("baz" "conf") (funcall f-send-multiplexer "baz" 'forget)))
+    (should (equal 2 prompted-times))))
 
+(ert-deftest tslime-make-send-dispatch-test ()
+  (let* ((f-send-screen (lambda (s &optional forget) (list 'screen s)))
+         (f-send-tmux (lambda (s &optional forget) (list 'tmux s)))
+         (f-send-dispatch (tslime-make-send-dispatch f-send-screen f-send-tmux)))
+    (should (equal '(tmux "foo") (funcall f-send-dispatch 'tmux "foo")))
+    (should (equal '(screen "bar") (funcall f-send-dispatch 'screen "bar")))
+    (should-error (funcall f-send-dispatch 'wrong "bar"))))
--- tslime.el
+++ tslime.el
@@ -1,8 +1,7 @@
+;; -*- lexical-binding: t -*-
 (defvar tslime-multiplexer 'tmux)
-(defvar tslime-screen-configuration nil)
-(defvar tslime-tmux-configuration nil)
 
-(defun tslime-str-join (separator seq)
+(defun tslime-s-join (separator seq)
   (mapconcat 'identity seq separator))
 
 (defun tslime-re-matches (re s)
@@ -12,75 +11,77 @@
 (defun tslime-append-newline-if-missing (s)
   (replace-regexp-in-string "\n\n$" "\n" (concat s "\n")))
 
-(defun tslime-paste-buffer-to-tmux-panel (fn-shell buffer panel)
-  (funcall fn-shell (tslime-str-join " " (list "tmux -L default load-buffer" buffer)))
-  (funcall fn-shell (tslime-str-join " " (list "tmux -L default paste-buffer -d -t" panel))))
+(defun tslime-paste-buffer-to-tmux-panel (f-shell buffer panel)
+  (funcall f-shell (tslime-s-join " " (list "tmux -L default load-buffer" buffer)))
+  (funcall f-shell (tslime-s-join " " (list "tmux -L default paste-buffer -d -t" panel))))
 
-(defun tslime-send-string-to-tmux-panel (s conf)
+(defun tslime-send-to-tmux-panel (s conf)
   (with-temp-file (cdr (assoc 'buffer conf)) (insert s))
   (apply 'tslime-paste-buffer-to-tmux-panel (cons 'shell-command (mapcar 'cdr conf))))
 
-(defun tslime-prompt-for-tmux-configuration ()
+(defun tslime-prompt-for-tmux-conf ()
   (list (cons 'buffer (make-temp-file "tslime-tmux"))
         (cons 'panel (read-string "Type panel id: " "{last}"))))
 
-(defun tslime-send-string-tmux (s)
-  (unless tslime-tmux-configuration
-    (setq tslime-tmux-configuration (tslime-prompt-for-tmux-configuration)))
-  (tslime-send-string-to-tmux-panel s tslime-tmux-configuration))
+(defun tslime-paste-buffer-to-screen-panel (f-shell buffer session region)
+  (funcall f-shell (tslime-s-join " " (list "screen -S" session "-X eval" (concat "'readreg p " buffer "'"))))
+  (funcall f-shell (tslime-s-join " " (list "screen -S" session "-p" region "-X paste p"))))
 
-(defun tslime-paste-buffer-to-screen-panel (fn-shell buffer session region)
-  (funcall fn-shell (tslime-str-join " " (list "screen -S" session "-X eval" (concat "'readreg p " buffer "'"))))
-  (funcall fn-shell (tslime-str-join " " (list "screen -S" session "-p" region "-X paste p"))))
-
-(defun tslime-send-string-to-screen-panel (s conf)
+(defun tslime-send-to-screen-panel (s conf)
   (with-temp-file (cdr (assoc 'buffer conf)) (insert s))
   (apply 'tslime-paste-buffer-to-screen-panel (cons 'shell-command (mapcar 'cdr conf))))
 
 (defun tslime-parse-screen-session-list (s)
-  (mapcan (lambda (x) (unless (null x) (list x)))
+  (mapcan (lambda (x) (when x (list x)))
           (mapcar (lambda (x) (tslime-re-matches "[0-9]+\\.[^ \t]+\\.[^ \t]+" x)) (split-string s "\n"))))
 
 (defun tslime-prompt-for-screen-session ()
   (let ((sessions (tslime-parse-screen-session-list (shell-command-to-string "screen -ls"))))
     (if sessions (completing-read "Choose session: " sessions) (user-error "No screen session found"))))
 
-(defun tslime-prompt-for-screen-configuration ()
+(defun tslime-prompt-for-screen-conf ()
   (list (cons 'buffer (make-temp-file "tslime-screen"))
         (cons 'session (tslime-prompt-for-screen-session))
         (cons 'region (read-string "Type region id: "))))
 
-(defun tslime-send-string-screen (s)
-  (unless tslime-screen-configuration
-    (setq tslime-screen-configuration (tslime-prompt-for-screen-configuration)))
-  (tslime-send-string-to-screen-panel s tslime-screen-configuration))
+(defun tslime-make-send-multiplexer (f-prompt-conf f-multiplexer-send)
+  (let ((conf nil))
+    (lambda (s &optional forget)
+      (when (or (not conf) forget) (setq conf (funcall f-prompt-conf)))
+      (funcall f-multiplexer-send s conf))))
 
-(defun tslime-send-string-dispatch (s)
-  (pcase tslime-multiplexer
-    ('screen (tslime-send-string-screen s))
-    ('tmux (tslime-send-string-tmux s))
-    (_ (user-error "Unsupported terminal multiplexer %s" tslime-multiplexer))))
+(defun tslime-make-send-dispatch (f-send-screen f-send-tmux)
+  (lambda (multiplexer s &optional forget)
+    (pcase multiplexer
+      ('screen (funcall f-send-screen s forget))
+      ('tmux (funcall f-send-tmux s forget))
+      (_ (user-error "Unsupported terminal multiplexer %s" multiplexer)))))
 
-(defun tslime-send-region ()
+(defconst tslime-send-screen
+  (tslime-make-send-multiplexer #'tslime-prompt-for-screen-conf #'tslime-send-to-screen-panel))
+(defconst tslime-send-tmux
+  (tslime-make-send-multiplexer #'tslime-prompt-for-tmux-conf #'tslime-send-to-tmux-panel))
+(defconst tslime-send-dispatch
+  (tslime-make-send-dispatch tslime-send-screen tslime-send-tmux))
+
+(defun tslime-send-region (multiplexer &optional forget)
   (let ((s (buffer-substring-no-properties (region-beginning) (region-end))))
-    (tslime-send-string-dispatch (tslime-append-newline-if-missing s))))
+    (funcall tslime-send-dispatch multiplexer (tslime-append-newline-if-missing s) forget)))
 
-(defun tslime-send ()
+(defun tslime-send (&optional forget)
   (interactive)
   (let ((pos (point)))
     (unless (use-region-p)
       (mark-paragraph))
-    (tslime-send-region)
+    (tslime-send-region tslime-multiplexer forget)
     (deactivate-mark)
     (goto-char pos)))
 
 (defun tslime-forget-send ()
   (interactive)
-  (setq tslime-screen-configuration nil)
-  (setq tslime-tmux-configuration nil)
-  (tslime-send))
+  (tslime-send 'forget))
 
-(global-set-key (kbd "C-c C-c") 'tslime-send)
-(global-set-key (kbd "C-c C-v") 'tslime-forget-send)
+(global-set-key (kbd "C-c C-c") #'tslime-send)
+(global-set-key (kbd "C-c C-v") #'tslime-forget-send)
 
 (provide 'tslime)
